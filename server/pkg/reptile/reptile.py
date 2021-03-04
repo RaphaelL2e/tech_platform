6a1aeb7cb1c0539eb3a03d5104519c08f7204899
liyafei <liyafei@sensetime.com>
2021-03-04 15:40:46 +0800

    fix: reptile to articles

--- server/pkg/reptile/reptile.py
+++ server/pkg/reptile/reptile.py
@@ -1,10 +1,10 @@
 import re
 import time
-
+import random
 import requests
 from sqlalchemy import Column, Integer, String, Time
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 base_url = "https://gateway.36kr.com/api/mis/nav/ifm/subNav/flow"
@@ -12,7 +12,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36",
     "content-type": "application/json"
 }
-initPageCallback = "eyJmaXJzdElkIjozMjMwNzM4LCJsYXN0SWQiOjMyMzAxNjAsImZpcnN0Q3JlYXRlVGltZSI6MTYxNDc1NzMzNjY0NSwibGFzdENyZWF0ZVRpbWUiOjE2MTQ3MzYxNDEwMTV9"
+initPageCallback = "eyJmaXJzdElkIjozMjMyMDY0LCJsYXN0SWQiOjMyMzEzNzgsImZpcnN0Q3JlYXRlVGltZSI6MTYxNDgzOTkwMDI4NywibGFzdENyZWF0ZVRpbWUiOjE2MTQ4MTU5ODA4MjV9"
 body = {
     "partner_id": "web",
     "timestamp": time.time(),
@@ -29,21 +29,22 @@ body = {
 Base = declarative_base()
 
 
-class Technology(Base):
-    __tablename__ = 'technologies'
+class Article(Base):
+    __tablename__ = 'articles'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    title = Column(String)
     summary = Column(String)
     image = Column(String)
-    image_type = Column(Integer)
-    context = Column(String)
     user_id = Column(String)
+    author = Column(String)
+    context = Column(String)
+    status = Column(Integer)
     create_at = Column(Time)
     update_at = Column(Time)
 
     def __repr__(self):
-        return "<Technology(id='%s',name='%s', summary='%s', image='%s',image_type='%s',context='%s', user_id='%s', create_at='%s',update_at='%s')>" % (
-            self.id, self.name, self.summary, self.image, self.image_type, self.context, self.user_id, self.create_at,
+        return "<Article(id='%s',title='%s', summary='%s', image='%s',user_id='%s', author='%s', context='%s', create_at='%s',update_at='%s')>" % (
+            self.id, self.title, self.summary, self.image, self.user_id, self.author, self.context, self.create_at,
             self.update_at)
 
 
@@ -76,7 +77,7 @@ if __name__ == '__main__':
     body["param"]["pageCallback"] = newPageCallback
     j = 0
     t = 0
-    while j < 10:
+    while j < 50:
         j += 1
         list, newPageCallback = getList(body)
         body["param"]["pageCallback"] = newPageCallback
@@ -85,11 +86,12 @@ if __name__ == '__main__':
             t += 1
             print(t)
             item = i["templateMaterial"]
-            name = item["widgetTitle"]
+            title = item["widgetTitle"]
             summary = item["summary"]
             create_at = item["publishTime"]
             image = item["widgetImage"]
             context_url = "https://www.36kr.com/p/" + str(item["itemId"])
+            author = item["authorName"]
             text = requests.get(context_url).text
             # text = text.replace("\\", "")
             list = re.compile("<p>(.*?)</p>").findall(text)
@@ -98,5 +100,7 @@ if __name__ == '__main__':
                 context_text += '<p>' + l + '</p>\n'
             context = context_text
             user_id = "371310968510091264"
-            ed_tech = Technology(name=name,summary=summary,image=image,image_type=1,context=context,user_id=user_id,create_at=time.localtime(create_at/1000),update_at=time.localtime())
+            ed_tech = Article(title=title, summary=summary, image=image, context=context,
+                              user_id=user_id, author=author, status=random.randint(-1,1), create_at=time.localtime(create_at / 1000),
+                              update_at=time.localtime())
             insertTech(engine, ed_tech)
